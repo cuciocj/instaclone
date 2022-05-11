d2b339a1429f3c22c1227be094dee107a23959d6
Christopher John Cucio <christopher.cucio@awsys-i.com>
2022-05-11 16:32:01 +0800

    removed windows and web

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:instaclone/routes/routes.dart';
 
@@ -23,6 +22,7 @@ class MyApp extends StatelessWidget {
           elevation: 1,
         ),
         textTheme: GoogleFonts.comicNeueTextTheme(Theme.of(context).textTheme),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
     );
   }
