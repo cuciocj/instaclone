f84bfbcec51e575e6cc5561169960917c2e333da
Christopher John Cucio <christopher.cucio@awsys-i.com>
2022-05-17 16:32:14 +0800

    [update] added new pages

---
+++ lib/screens/reels_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ReelPage extends StatelessWidget {
+  const ReelPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('reels page'),
+    );
+  }
+}
